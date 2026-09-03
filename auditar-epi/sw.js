9b2b7e76a3d2411ad3feb83b44a95c0a1d556216
tstauditarsolucoes-tech <tstauditarsolucoes@gmail.com>
2026-09-03 19:41:03 -0300

    Atualiza cache para assinatura horizontal em tela cheia

--- auditar-epi/sw.js
+++ auditar-epi/sw.js
@@ -1,4 +1,4 @@
-const CACHE='auditar-epi-v9-prova-vida';
+const CACHE='auditar-epi-v10-assinatura-horizontal';
 const ASSETS=['./','./index.html','./styles.css','./import-workers.css','./stock.css','./app.js','./import-workers.js','./ai-workers.js','./stock.js','./signature-assist.js','./biometric-face.js','./liveness-face.js','./cloud-sync.js','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
