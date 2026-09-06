583ef4893c4bdc3713074845ca5ad8c81846958b
tstauditarsolucoes-tech <tstauditarsolucoes@gmail.com>
2026-09-06 13:07:51 -0300

    Mostra versão PC v2.7.5

--- auditar-epi-windows/pc-runtime-fix.js
+++ auditar-epi-windows/pc-runtime-fix.js
@@ -7,7 +7,7 @@
       s.textContent=`
         .sidebar-foot{position:relative;padding-bottom:28px!important}
         .sidebar-foot::after{
-          content:'PC v2.7.4';
+          content:'PC v2.7.5';
           position:absolute;
           left:28px;
           bottom:8px;
